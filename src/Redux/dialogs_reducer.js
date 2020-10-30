4c0bc17be5afccbdd9b7cf615dd9f56673103713
dav_av <adavid0707@gmail.com>
2020-10-31 01:09:54 +0400

    das48 2

--- src/Redux/dialogs_reducer.js
+++ src/Redux/dialogs_reducer.js
@@ -1,48 +1,46 @@
 const ADD_MESSAGE = "ADD-MESSAGE";
 const UPDATE_DIALOG_TEXT = "UPDATE-DIALOG-TEXT";
-export const addMessageActionCreator = ()=>({type:ADD_MESSAGE})
-export const dialogMessageTextActionCreator = (message)=>({
-  type:UPDATE_DIALOG_TEXT, message:message
-})
+export const addMessageActionCreator = () => ({ type: ADD_MESSAGE });
+export const dialogMessageTextActionCreator = (message) => ({
+  type: UPDATE_DIALOG_TEXT,
+  message: message,
+});
 let initialState = {
-    messagesData: [
-        { id: 1, message: "sdsdsdsd" },
-        { id: 2, message: "sdsdsdsd" },
-        { id: 3, message: "sdsdsdsd" },
-        { id: 4, message: "sdsdsdsd" },
-        { id: 5, message: "sdsdsdsd" },
-      ],
-      dialogsData: [
-        { id: 1, name: "David" },
-        { id: 2, name: "Andrew" },
-        { id: 3, name: "Sveta" },
-        { id: 4, name: "Sasha" },
-        { id: 5, name: "Valera" },
-      ],
-      dialogMessage: "gri"
-}
+  messagesData: [
+    { id: 1, message: "sdsdsdsd" },
+    { id: 2, message: "sdsdsdsd" },
+    { id: 3, message: "sdsdsdsd" },
+    { id: 4, message: "sdsdsdsd" },
+    { id: 5, message: "sdsdsdsd" },
+  ],
+  dialogsData: [
+    { id: 1, name: "David" },
+    { id: 2, name: "Andrew" },
+    { id: 3, name: "Sveta" },
+    { id: 4, name: "Sasha" },
+    { id: 5, name: "Valera" },
+  ],
+  dialogMessage: "gri",
+};
 const dialogsReducer = (state = initialState, action) => {
-    switch(action.type){
-        case ADD_MESSAGE:{
-            let newMessage = {
-                id: 6,
-                message: state.dialogMessage,
-              };
-              let stateCopy = {...state,
-                messagesData:[...state.messagesData]
-              }
-              stateCopy.messagesData.push(newMessage);
-              stateCopy.dialogMessage = "";
-              return stateCopy;
-            }
-              case UPDATE_DIALOG_TEXT:{
-                let stateCopy = {...state}
-                stateCopy.dialogMessage = action.message;
-                return stateCopy;
-              }
-                default:
-                    return state;
+  switch (action.type) {
+    case ADD_MESSAGE: {
+      let newMessage = {
+        id: 6,
+        message: state.dialogMessage,
+      };
+      return {
+        ...state,
+        messagesData: [...state.messagesData, newMessage],
+        dialogMessage: "",
+      };
     }
+    case UPDATE_DIALOG_TEXT: {
+      return { ...state, dialogMessage: action.message };
+    }
+    default:
+      return state;
+  }
 };
 
 export default dialogsReducer;
