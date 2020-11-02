91c2561700865971c4a151c7fced4f4685e16f51
dav_av <adavid0707@gmail.com>
2020-11-03 00:40:42 +0400

    das63 DAL sarqecinq

---
+++ src/API/api.js
@@ -0,0 +1,30 @@
+
+import * as axios from "axios"
+const baseUrl = `https://social-network.samuraijs.com/api/1.0/`;
+
+const instance = axios.create({
+    withCredentials:true,
+    baseURL: `https://social-network.samuraijs.com/api/1.0/`,
+    headers:{
+        "API-KEY" : "560961f6-c212-4142-b02d-d99d0a8308d3"
+      }
+})
+
+export const usersApi = {
+     getUsers(currentPage ,pageSize){
+
+        return instance
+           .get(
+             `users?page=${currentPage}&count=${pageSize}`,{withCredentials:true}
+           ).then(response=>response.data)
+     }
+}
+
+
+// export const getUsers = (currentPage ,pageSize)=>{
+
+//    return instance
+//       .get(
+//         `users?page=${currentPage}&count=${pageSize}`,{withCredentials:true}
+//       ).then(response=>response.data)
+// }
--- src/components/Users/Users_Container.jsx
+++ src/components/Users/Users_Container.jsx
@@ -10,21 +10,20 @@ import {
 } from "../../Redux/users_reducer";
 import * as axios from "axios";
 import Users from "./Users";
-import preloader from "../../assets/images/45.svg";
 import Preloader from "../Preloader/Preloader";
+import { usersApi } from "../../API/api";
+
 
 class UsersContainer extends React.Component {
   componentDidMount() {
     this.props.setIsFetching(true);
-    axios
-      .get(
-        `https://social-network.samuraijs.com/api/1.0/users?page=${this.props.currentPage}&count=${this.props.pageSize}`,{withCredentials:true}
-      )
+
+    usersApi.getUsers(this.props.currentPage, this.props.pageSize)
       .then((response) => {
         return (
-          this.props.setUsers(response.data.items),
+          this.props.setUsers(response.items),
           this.props.setIsFetching(false),
-          this.props.setTotalUsersCount(response.data.totalCount)
+          this.props.setTotalUsersCount(response.totalCount)
         
         );
       });
@@ -32,13 +31,10 @@ class UsersContainer extends React.Component {
   onPageChanged = (pageNumber) => {
     this.props.setCurrentPage(pageNumber);
     this.props.setIsFetching(true);
-    axios
-      .get(
-        `https://social-network.samuraijs.com/api/1.0/users?page=${pageNumber}&count=${this.props.pageSize}`,{withCredentials:true}
-      )
+    usersApi.getUsers(pageNumber,this.props.pageSize)
       .then((response) => {
         return (
-          this.props.setUsers(response.data.items),
+          this.props.setUsers(response.items),
 
           this.props.setIsFetching(false)
         );
@@ -71,37 +67,7 @@ let mapStateToProps = (state) => {
     isFetching: state.usersPage.isFetching,
   };
 };
-// let mapDispatchToProps = (dispatch) => {
-//   return {
-//     follow: (userId) => {
-//       dispatch(followAC(userId));
-//     },
-//     unFollow: (userId) => {
-//       dispatch(unFollowAC(userId));
-//     },
-//     setUsers: (users) => {
-//       dispatch(setUsersAC(users));
-//     },
-//     setCurrentPage: (pageNumber) => {
-//       dispatch(setCurrentPageAC(pageNumber));
-//     },
-//     setTotalUsersCount: (totalCount) => {
-//       dispatch(setUsersTotalCountAC(totalCount));
-//     },
-//     toggleIsFetching: (isFetching) => {
-//       dispatch(setIsFetchingAC(isFetching));
-//     },
-//   };
-// };
-// let mapDispatchToProps1 =
-//    {
-//     follow: followAC,
-//     unFollow: unFollowAC,
-//     setUsers: setUsersAC,
-//     setCurrentPage: setCurrentPageAC,
-//     setTotalUsersCount:setUsersTotalCountAC,
-//     toggleIsFetching: setIsFetchingAC
-//     }
+
 
 export default connect(mapStateToProps, {
   follow,
