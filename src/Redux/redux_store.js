1796ea11c82604ef6d8f6e30b9944688b7cd2aba
unknown <adavid0707@gmail.com>
2020-11-17 16:48:39 +0300

    das75 redux-form

--- src/Redux/redux_store.js
+++ src/Redux/redux_store.js
@@ -4,12 +4,14 @@ import authReducer from "./auth-reducer";
 import dialogsReducer from "./dialogs_reducer";
 import profileReducer from "./profile_reducer";
 import usersReducer from "./users_reducer";
+import { reducer as formReducer } from 'redux-form'
 
 let reducers = combineReducers({
     profilePage: profileReducer,
     DialogsPage: dialogsReducer,
     usersPage:usersReducer,
-    auth:authReducer
+    auth:authReducer,
+    form:formReducer
 });
 
 let store = createStore(reducers,applyMiddleware(thunkMiddleware));
--- src/components/login/login.jsx
+++ src/components/login/login.jsx
@@ -1,9 +1,56 @@
 import React from 'react'
+import { Field, reduxForm } from 'redux-form'
 
-const Login = ()=>{
+const LoginForm = (props)=>{
+
     return(
-        <h1>LOGIN</h1>
+        <>
+            
+            <form onSubmit = {props.handleSubmit} >
+                <div>
+                    <Field type="text" placeholder={"Login"}
+                        component = {"input"}
+                        name = {"Username"}
+                    />
+                </div>
+                <div>
+                <Field type="password" placeholder={"Password"}
+                component = {"input"}
+                name = {"Password"}
+                />
+                </div>
+                <div>
+                <Field type={"checkbox"}
+                    name={"remember me"}
+                    component={"input"}
+                /> remeber me
+                </div>
+                <div>
+                    <button>Log In</button>
+                </div>
+            </form>
+        </>
     )
 }
+const LoginReduxForm = reduxForm(
+    {
+        form:"Login"
+    }
+)(LoginForm)
+
+
+const Login = (props)=>{
+    const onSubmit = (formData) =>{
+        console.log(formData);
+    }
+    return(
+        <>
+            <h1>Login</h1>
+            <LoginReduxForm onSubmit = {onSubmit}/>
+        </>
+    )
+}
+
+
 
 export default Login
