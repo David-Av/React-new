73c3e1d895a252340a4a833912e501d709fd285e
unknown <adavid0707@gmail.com>
2020-11-25 22:45:20 +0400

    91 extencions

--- src/Redux/redux_store.js
+++ src/Redux/redux_store.js
@@ -1,4 +1,4 @@
-import { applyMiddleware, combineReducers, createStore } from "redux";
+import { applyMiddleware, combineReducers, compose, createStore } from "redux";
 import  thunkMiddleware from "redux-thunk";
 import authReducer from "./auth-reducer";
 import dialogsReducer from "./dialogs_reducer";
@@ -6,6 +6,13 @@ import profileReducer from "./profile_reducer";
 import usersReducer from "./users_reducer";
 import { reducer as formReducer } from 'redux-form'
 import appReducer from "./app_reducer";
+
+
+
+
+
+
+
 
 let reducers = combineReducers({
     profilePage: profileReducer,
@@ -15,7 +22,10 @@ let reducers = combineReducers({
     form:formReducer,
     app:appReducer
 });
+const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+const store = createStore(reducers, /* preloadedState, */ composeEnhancers(applyMiddleware(thunkMiddleware)
+  ));
 
-let store = createStore(reducers,applyMiddleware(thunkMiddleware));
-window.store = store
+// let store = createStore(reducers,applyMiddleware(thunkMiddleware));
+window.___store____ = store
 export default store;
